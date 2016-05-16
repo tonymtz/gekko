9b5c6d5de5b138cf93fa3997327ec83d4a38042c
tonymtz <tmartinez@switchfly.com>
2016-05-15 18:59:24 -0700

    fixed dropbox urls

--- services/oauth2/providers/dropbox.go
+++ services/oauth2/providers/dropbox.go
@@ -3,8 +3,8 @@ package provider
 import "github.com/tonymtz/gekko/services/oauth2"
 
 const (
-	DROPBOX_AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/v2/auth?client_id=%v&response_type=code&redirect_uri=%v&scope=email profile"
-	DROPBOX_TOKEN_EXCHANGE_URL = "https://www.googleapis.com/oauth2/v4/token"
+	DROPBOX_AUTHORIZATION_URL = "https://www.dropbox.com/oauth2/authorize?client_id=%v&response_type=code&redirect_uri=%v"
+	DROPBOX_TOKEN_EXCHANGE_URL = "https://api.dropboxapi.com/oauth2/token"
 )
 
 type Dropbox struct {
